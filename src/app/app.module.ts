00cbc3efe404549426cb416af7eda985acbcbc04
Shadocks <mickael.bardeau@laposte.net>
2018-10-01 15:25:12 +0200

    angular(practice): solved exercises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { Exercice1Component } from './exercice1/exercice1.component';
 import { Exercice2Component } from './exercice2/exercice2.component';
+import { Exercice3Component } from './exercice3/exercice3.component';
+import { Exercice4Component } from './exercice4/exercice4.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    Exercice2Component
+    HeaderComponent,
+    Exercice1Component,
+    Exercice2Component,
+    Exercice3Component,
+    Exercice4Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -12,4 +12,7 @@ export class Exercice2Component implements OnInit {
   ngOnInit() {
   }
 
+  iClick(): void {
+    console.log('clic');
+  }
 }
